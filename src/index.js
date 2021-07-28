d71c55a3a243ae86902a71b7f42dae95c793d4bb
Potears <ryan.potere@gmail.com>
2021-07-27 23:16:36 -0400

    html generates and can continue to add employees one after another.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const fs = require("fs");
 
 const newEmployee = [];
 
-
-menu = () => {
     createManager = () => {
         inquirer.prompt([
             {
@@ -32,14 +30,28 @@ menu = () => {
                 name: "officeNumber",
                 message: "Enter Manager's office number:",
 
-            }
-        ]).then(({ name, id, email, officeNumber }) => {
+            },  
+            {
+                type: "list",
+                name: "createMore",
+                message: "Would you like create another profile?",
+                choices:["Yes", "No"]
+    
+            },
+        ]).then(({ name, id, email, officeNumber, createMore }) => {
+            if (createMore === 'Yes') {
             const manager = new Manager(name, id, email, officeNumber);
+            newEmployee.push(manager);
+            console.log(newEmployee);
+            createProfile();
+            } else {
+                const manager = new Manager(name, id, email, officeNumber);
             newEmployee.push(manager);
             console.log(newEmployee);
             const fileName = `index.html`;
             const htmlString = convertPrompts(manager);
             printHtml(fileName, htmlString);
+            }
         })
     }
 
@@ -67,17 +79,31 @@ menu = () => {
                 name: "gitHub",
                 message: "Enter Engineer's GitHub username:",
 
-            }
-        ]).then(({ name, id, email, gitHub }) => {
+            },
+            {
+                type: "list",
+                name: "createMore",
+                message: "Would you like create another profile?",
+                choices:["Yes", "No"]
+    
+            },
+        ]).then(({ name, id, email, gitHub, createMore }) => {
+            if (createMore === 'Yes') {
+            const engineer = new Engineer(name, id, email, gitHub);
+            newEmployee.push(engineer)
+            console.log(engineer);
+            createProfile();
+            } else {
             const engineer = new Engineer(name, id, email, gitHub);
             newEmployee.push(engineer)
             console.log(engineer);
             const fileName = `index.html`;
             const htmlString = convertPrompts(engineer);
             printHtml(fileName, htmlString);
+            }
         })
     }
-}
+
 
 createIntern = () => {
     inquirer.prompt([
@@ -103,17 +129,34 @@ createIntern = () => {
             name: "school",
             message: "Enter school name:",
 
-        }
-    ]).then(({ name, id, email, school }) => {
+        },  
+        {
+            type: "list",
+            name: "createMore",
+            message: "Would you like create another profile?",
+            choices:["Yes", "No"]
+
+        },
+    ]).then(({ name, id, email, school, createMore}) => {
+
+        if (createMore === 'Yes') {
         const intern = new Intern(name, id, email, school);
         console.log("SCHOOl:" + intern.getSchool());
         intern.getRole();
         console.log(intern);
         newEmployee.push(intern);
-        const fileName = `index.html`;
-        const htmlString = convertPrompts(intern);
-        printHtml(fileName, htmlString);
+        createProfile();
+        } else {
+            const intern = new Intern(name, id, email, school);
+            console.log("SCHOOl:" + intern.getSchool());
+        intern.getRole();
+        console.log(intern);
+        newEmployee.push(intern);
+            const fileName = `index.html`;
+            const htmlString = convertPrompts(intern);
+            printHtml(fileName, htmlString);
         // console.log(htmlString);
+        }
     })
 };
 
@@ -123,7 +166,7 @@ createProfile = () => {
             type: "list",
             name: "employeeType",
             message: "what type of employee profile would you like to create?",
-            choices: ['Manager', 'Engineer', 'Intern', 'I do not want to create another profile'],
+            choices: ['Manager', 'Engineer', 'Intern', 'Exit'],
         }]).then((data) => {
             if (data.employeeType === 'Manager') {
                 createManager();
@@ -136,10 +179,9 @@ createProfile = () => {
         })
 }
 
-createProfile();
 
 printHtml = (fileName, htmlString) => {
-    fs.writeFileSync(fileName, htmlString, { flag: "a" });
+    fs.writeFileSync(fileName, htmlString);
 
 }
 
@@ -208,5 +250,4 @@ ${printIndex}
 };
 
 
-
-menu();
+createProfile();
